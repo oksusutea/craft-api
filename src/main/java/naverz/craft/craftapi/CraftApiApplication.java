d233b333302fafc2a83f0597c92555dfc8ae675c
oksusutea <chanjukim118@gmail.com>
2021-05-26 21:07:10 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/naverz/craft/craftapi/CraftApiApplication.java
@@ -0,0 +1,13 @@
+package naverz.craft.craftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/naverz/craft/craftapi/CraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package naverz.craft.craftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
